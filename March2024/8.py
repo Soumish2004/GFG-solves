09aded162a988b0823816a7dd137daa69301e5da
Soumish2004 <soumish2004flamesaver@gmail.com>
2024-03-08 16:09:29 +0530

    8th March

--- March2024/8.py
+++ March2024/8.py
@@ -9,7 +9,22 @@ class Solution:
             cCount[char] +=1
         Count = [i for i in cCount.values()]
         mCount = min(Count)
+        prev=-1
+        for i in Count:
+            if i!=1:
+                prev = i
+                break;
+        if prev==-1:
+            return 1;
+        count = oCount =0;
         rCount = 0
+        for i in Count:
+            if i==1:
+                oCount+=1;
+            if prev == i:
+                count+=1
+        if count == len(Count)-1 and oCount==1:
+            return 1;
         for i in Count:
             rCount += i-mCount
             if(rCount>1):
